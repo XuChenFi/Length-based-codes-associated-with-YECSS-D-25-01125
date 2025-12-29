dcfcb1c11421d9b8f95cdc3140b56b22201d5966
XuChenFi <xuchen.xcxc@outlook.com>
2025-12-29 21:37:11 +0800

    Update Multi-year LBSPR running.R

--- LBSPR/Multi-year LBSPR running.R
+++ LBSPR/Multi-year LBSPR running.R
@@ -27,6 +27,27 @@ result[is.na(result)] <- 0
 
 # Sort by Length
 result <- result[order(result$Length), ]
-
+write.table(result, "C:/data.csv", 
+            col.names = FALSE, 
+            sep = ",")
 # Rename first column to "Length_Group_Median"
 colnames(result)[1] <- "Length_Group_Median"
+#LBSPR running
+library(LBSPR)
+LB_pars <- new("LB_pars")
+LB_pars@MK <- 0.57/0.272324733
+LB_pars@Linf <- max(result[,1])
+LB_pars@L50 <- 11
+LB_pars@L95 <- 12
+LB_pars@CVLinf <- 0.15
+LB_pars@FecB <- 3
+LB_pars@Mpow <- 0
+LB_pars@Walpha <- 0.231
+LB_pars@Wbeta <- 2.36
+LB_pars@BinWidth <- 1
+Len1 <- new("LB_lengths", LB_pars=LB_pars, file=paste0("C:/data.csv"),
+dataType="freq")
+Len2 <- new("LB_lengths", LB_pars=LB_pars, file=paste0("C:/data.csv"),
+dataType="raw")
+lbspr_res <- LBSPRfit(LB_pars=LB_pars, LB_lengths=Len2, Control=list(modtype=c("GTG")))
+spr <- lbspr_res@SPR
